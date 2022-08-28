b7fc0b38fa71338beae1b80c469dac17d3f96725
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2022-08-28 14:34:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from distutils.log import error
+from logging.config import dictConfig
+from tokenize import Number
+from unicodedata import numeric
 from flask import Flask, request, render_template, redirect, url_for,session
 from cliente_app import conexao_cliente_primeira_msg
 from modelo_db import connection_db
@@ -6,6 +9,8 @@ from modelo_pusher import connection_pusher
 import psycopg2
 import time
 import pusher
+
+from server.pegardado import pegardado2
 
  
 pusher_client = pusher.Pusher(
@@ -23,8 +28,70 @@ app.config['SECRET_KEY'] = 'testesecret'
 
 @app.route('/')
 def index():
-
+    
     return render_template('index.html', url_for=url_for)
+
+@app.route('/disciplina_retornar_topico', methods=['POST'])
+def disciplina_retornar_topico():
+    dicio = pegardado2()
+    try:
+        disciplina = request.form['disciplina']
+        print(disciplina)
+        lista_assunto = dicio[dicio['disciplina']==disciplina]['assunto'].unique()
+        
+        
+        return list(lista_assunto)
+    except Exception as e:
+        print(e)
+        return 'fail'
+
+@app.route('/teste2', methods=['POST'])
+def teste2():
+
+    nome_from = request.form['nome']
+    # print('-----')
+    # print(nome_from)
+
+    try:
+        connection_db().insert_user(nome_from)
+        #session['msg_from'] = nome
+        # print('[----Usuário inserido-----]')
+    except Exception as e:
+        #session['msg_from'] = nome
+        # print(e)
+        # print("Não inserido --")
+        # msg_true = True
+        # msg = 'Usuário já cadastrado'
+
+        # return render_template('index.html', url_for=url_for,msg=msg,msg_true=msg_true)
+        # print('[---Usuário já consta no BD]---')
+        # print(e)
+        pass
+
+    try:    
+        userid = connection_db().gel_userid(nome_from)
+        # print('[---userid---]: ',userid)
+    except:
+        # print('[---Error em pegar o ID---]')
+        pass
+    try:
+        total_users = connection_db().get_all_users_minus(userid)
+        # print(total_users)
+    except:
+        # print('[---Error ao pegar a lista menos o atual]---')
+        pass
+
+
+    df = pegardado2()
+    disciplinas = df['disciplina'].unique()
+
+    
+    userid = connection_db().gel_userid(nome_from)
+
+    total_users = connection_db().get_all_users_minus(userid)
+    # print(total_users)
+
+    return render_template('teste2.html',disciplinas=disciplinas,total_users=total_users,nome_from=nome_from)
 
 @app.route('/enviar_nome', methods=['POST'])
 def enviar_nome():
@@ -35,7 +102,7 @@ def enviar_nome():
     try:
         connection_db().insert_user(nome)
         #session['msg_from'] = nome
-
+        print('[----Usuário inserido-----]')
     except Exception as e:
         #session['msg_from'] = nome
         # print(e)
@@ -44,7 +111,8 @@ def enviar_nome():
         # msg = 'Usuário já cadastrado'
 
         # return render_template('index.html', url_for=url_for,msg=msg,msg_true=msg_true)
-        pass
+        print('[---Usuário já consta no BD]---')
+        print(e)
     lista_client = []
 
 
@@ -68,40 +136,19 @@ def enviar_nome():
     #print(msg[4])'
     msg[5] = msg[5].split('<br>')
 
-    userid = connection_db().gel_userid(nome)
-    total_users = connection_db().get_all_users_minus(userid)
-    print(total_users)
+    try:    
+        userid = connection_db().gel_userid(nome)
+        print('[---userid---]: ',userid)
+    except:
+        print('[---Error em pegar o ID---]')
+
+    try:
+        total_users = connection_db().get_all_users_minus(userid)
+        print(total_users)
+    except:
+        print('[---Error ao pegar a lista menos o atual]---')
 
     return render_template('teste.html', nome=nome, msg=msg, total_users=total_users, len=len,userid=userid[0][0])
-
-
-# @app.route('/set_conversa', methods=['POST'])
-# def set_conversa():
-
-#     msg_from =  request.form['msg_from']
-#     msg_from_i = int(connection_db().gel_userid(msg_from)[0][0])
-#     print(msg_from)
-
-#     msg_to = request.form['msg_to']
-#     msg_to_i =int(connection_db().gel_userid(msg_to)[0][0])
-#     session['msg_to'] = msg_to
-
-#     if msg_from_i < msg_to_i:
-#         evento = f'{msg_from_i}_evento_{msg_to_i}'
-#     else:
-#         evento = f'{msg_to_i}_evento_{msg_from_i}'
-
-
-#     dicio = {'evento':evento,'name_from':msg_from,'name_from_i':msg_from_i,'name_to:':msg_to,'name_to_i':msg_to_i}
-    
-#     # try:
-#     #     connection_pusher(evento,dicio)
-
-#     #     return dicio
-#     # except Exception as e:
-#     #     print(e)
-#     #     return 'fail'
-#     return dicio
 
 @app.route('/chat_client', methods = ['POST'])
 def chat_client():
@@ -142,8 +189,89 @@ def chat_client():
         print(e)
         return 'fail'
 
-    # return dicio
-    # evento = 
+@app.route('/link_questao', methods=['POST'])
+def link_questao():
+
+    df = pegardado2()
+    disciplina = request.form['disciplina']
+    topico = request.form['topico']
+    quantidade = int(request.form['quantidade'])
+    momento = request.form['momento']
+    nome_from = request.form['nome_from']
+    i_nome_from = connection_db().gel_userid(nome_from)
+    print('-------')
+    i_nome_from = int(i_nome_from[0][0])
+    # i_nome_from
+    nome_chat_to = request.form['nome_chat_to']
+    i_nome_chat_to = int(request.form['i_nome_chat_to'])
+
+    evento_chat = f'{i_nome_chat_to}_evento'
+
+    if i_nome_from < i_nome_chat_to:
+        evento = f'{i_nome_from}_evento_{i_nome_chat_to}'
+    else:
+        evento = f'{i_nome_chat_to}_evento_{i_nome_from}'
+
+
+    # print(disciplina)
+    # print(topico)
+    # print(quantidade)
+    print('---------------------------------------')
+    df1 = df[df['disciplina']==str(disciplina)]
+    # print(df1)
+    df2 = df1[df1['assunto'].str.strip().replace(' ','')==str(topico)]
+
+    try:
+       df3 = df2.sample(quantidade)
+       lista_geral_questoes = df3.values.tolist()
+    except Exception:
+        df3 = df2
+        lista_geral_questoes = df3.values.tolist()
+
+
+    topico = topico.strip().replace(' ','')
+
+
+    
+    link ='http://127.0.0.1:5000/'+disciplina+'/questao/'+topico+'/'+momento+'/_evento_/'+evento
+
+    dicio = {
+        'evento_chat':evento_chat,
+        'evento':evento,
+        'link':link,
+        'df':lista_geral_questoes,
+    }
+
+    return dicio
+
+@app.route('/<disciplina>/questao/<topico>/<momento>/_evento_/<evento>')
+def entender_link_gerar(disciplina,topico,momento,evento):
+    print(disciplina)
+    print(topico)
+    print(momento)
+    print(evento)
+
+    return 'ok'
+
+@app.route('/msg_chat_enviar', methods=['POST'])
+def msg_chat_enviar():
+    nome_from = request.form['nome_from']
+    evento_chat = request.form['evento_chat']
+    msg_a_enviar = request.form['msg_a_enviar']
+
+    dicio = {
+        'nome_from':nome_from,  
+        'evento_chat':evento_chat,  
+        'msg_a_enviar':msg_a_enviar,    
+
+    }
+    try:
+        connection_pusher(evento_chat,dicio)
+
+        return 'mensagem enviada ao Pusher'
+    except:
+
+        return 'mensagem nao enviada'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- server/pegardado.py
+++ server/pegardado.py
@@ -1,8 +1,16 @@
-
+import csv
+from dataclasses import replace
+from importlib.resources import path
+import os
+from tkinter.tix import COLUMN
+import pandas as pd
 def pegardado2():
-    import csv
-    with open("../server/lista_csv.csv",encoding='utf-8') as f:
-        reader = csv.reader(f)
-        lista = list(reader)
-    return lista
-pegardado2()
+    cwd = os.getcwd()         
+    path_ = os.path.join(cwd,'server','lista_csv.csv')
+    # with open(path_,encoding='utf-8') as f:
+    #     reader = csv.reader(f)
+    #     lista = list(reader)
+    # return lista
+    dicio = pd.read_csv(path_)
+    dicio = dicio.rename(columns={'Unnamed: 0':'cod_questao'})
+    return dicio
