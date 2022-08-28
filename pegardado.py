9ddefb7c61586992ade8f1065c0cd95de56123d7
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2022-08-28 14:40:01 -0300

    Add files via upload

---
+++ pegardado.py
@@ -0,0 +1,15 @@
+import csv
+from dataclasses import replace
+from importlib.resources import path
+import os
+import pandas as pd
+def pegardado2():
+    cwd = os.getcwd()         
+    path_ = os.path.join(cwd,'server','lista_csv.csv')
+    # with open(path_,encoding='utf-8') as f:
+    #     reader = csv.reader(f)
+    #     lista = list(reader)
+    # return lista
+    dicio = pd.read_csv(path_)
+    dicio = dicio.rename(columns={'Unnamed: 0':'cod_questao'})
+    return dicio
