7d545ae7a0cfbedf2ffa56173ded67ee189e844b
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2022-09-04 17:38:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -269,9 +269,9 @@ def link_questao():
 
 
     
-    # link ='http://127.0.0.1:5000/'+disciplina+'/questao/'+topico+'/'+str(quantidade)+'/'+momento+'/_evento_/'+evento+'/_i_/'+str(i_nome_from)
+    link ='http://127.0.0.1:5000/'+disciplina+'/questao/'+topico+'/'+str(quantidade)+'/'+momento+'/_evento_/'+evento+'/_i_/'+str(i_nome_from)
     
-    link ='https://projeto-questoes-chat-nixquest.herokuapp.com/'+disciplina+'/questao/'+topico+'/'+str(quantidade)+'/'+momento+'/_evento_/'+evento+'/_i_/'+str(i_nome_from)
+    # link ='https://projeto-questoes-chat-nixquest.herokuapp.com/'+disciplina+'/questao/'+topico+'/'+str(quantidade)+'/'+momento+'/_evento_/'+evento+'/_i_/'+str(i_nome_from)
     try:
         connection_db().insert_convite(evento,link,momento,nome_from,nome_chat_to,str(quantidade))
         print('[convite inserido no bd]')
@@ -371,6 +371,7 @@ def entender_link_gerar(disciplina,topico,quantidade,momento,evento,i_nome_from)
         'evento':evento,
     }
    
+    print(dicio_users)
     for i in range(0, len(lista)):
         lista[i][5] = lista[i][5].split('\n')          
 
